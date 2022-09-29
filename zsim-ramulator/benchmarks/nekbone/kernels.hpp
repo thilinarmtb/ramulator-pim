2af337c0884579d5dd3136538ae6b8a5fc6e20f5
Thilina Ratnayaka <thilinarmtb@gmail.com>
2022-09-29 17:48:16 +0000

    Change gs API

--- zsim-ramulator/benchmarks/nekbone/kernels.hpp
+++ zsim-ramulator/benchmarks/nekbone/kernels.hpp
@@ -6,6 +6,6 @@
 struct gs_data;
 struct gs_data *gs_setup(unsigned nelt, unsigned nx1, unsigned ndim,
                          unsigned verbose);
-int gs(scalar *u, unsigned n, unsigned *off, unsigned *id);
+void gs(scalar *u, struct gs_data *gsh);
 
 #endif
